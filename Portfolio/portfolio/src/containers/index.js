6f793ed49cf77b4505c3af0e38b0be3fbbda49c6
Nada F <nida.chm@gmail.com>
2022-04-24 11:24:14 +0100

    once more

--- Portfolio/portfolio/src/containers/index.js
+++
@@ -1,7 +0,0 @@
-export {default  as Blog } from "./blog/Blog";
-export {default as Projects }from "./projects/Projects";
-export { default as Footer } from "./footer/Footer";
-export { default as Header } from "./header/Header";
-export { default as Education } from "./education/Education";
-export { default as AboutMe} from "./aboutMe/AboutMe"
-export {default as ContactMe} from "./contactMe/ContactMe"
